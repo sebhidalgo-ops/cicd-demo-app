7e143ca19c9621bfef78bbf3a6e3d330be7cee7e
sebhidalgo-ops <sebhidalgo@hotmail.com>
2025-10-23 11:53:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/html'});
-  res.end(`<h1>Hello from Elastic Beanstalk CI/CD! pipeline.</h1>`);
+  res.end(`<h1>Hello from Elastic Beanstalk CI/CD!k pipeline.</h1>`);
 });
 server.listen(port, () => console.log(`Listening on ${port}`));
+
 
 
